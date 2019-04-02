a57aabd91bf2a8d6335a236e6d5a32ef96bff028
Justas <justaslau@gmail.com>
2019-04-01 21:30:39 -0400

    Updates

---
+++ precache-manifest.7a64e5e2d946faeab3edd007c75d9dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d8ff8f3f3eecf38ad1",
+    "url": "/ClickyGame/static/css/main.9011a95b.chunk.css"
+  },
+  {
+    "revision": "74d8ff8f3f3eecf38ad1",
+    "url": "/ClickyGame/static/js/main.2460eaff.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "a5cf51b22448f2398bc8",
+    "url": "/ClickyGame/static/css/2.5d6a9292.chunk.css"
+  },
+  {
+    "revision": "a5cf51b22448f2398bc8",
+    "url": "/ClickyGame/static/js/2.2b1044c8.chunk.js"
+  },
+  {
+    "revision": "3c7c80cb43f2dfc669c97ef81f51bcb8",
+    "url": "/ClickyGame/index.html"
+  }
+];
